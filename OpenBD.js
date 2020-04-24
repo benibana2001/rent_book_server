9d265b3b6a9d6cd66e0b555987c9f76c8ae107bc
benibana2001 <benibana2001@gmail.com>
2020-04-24 22:28:17 +0900

    [2020-04-24] implement to write JSON

--- OpenBD.js
+++ OpenBD.js
@@ -38,19 +38,71 @@ module.exports = class OpenBD {
   async downloadCoverImage(isbn) {
     const res = await this.search(isbn);
 
-    promiseFileDownload(res)
-      .then(promiseFileDownloadSuccess(this.imagePath))
-      .catch(promiseFileDownloadFail);
+    promiseFileDownload
+      .download(res)
+      .then(promiseFileDownload.success(this.imagePath))
+      .catch(promiseFileDownload.fail);
 
     return res;
   }
+
+  writeJSON(filepathname, res) {
+    const bookdata = {
+      isbn: res.isbn,
+      title: res.title,
+      publisher: res.publisher,
+      pubdate: res.pubdate,
+      cover: res.cover,
+      author: res.author,
+    };
+
+    // if there is no file, create new one.
+    syncIO.checkExist(filepathname, () => {
+      syncIO.write(filepathname, "[]");
+    });
+
+    syncIO.readAsJSON(filepathname, (json) => {
+      // postscript to JSON
+      json.push(bookdata);
+      syncIO.write(filepathname, JSON.stringify(json));
+      console.log(`write is done: ${JSON.stringify(json)}`);
+    });
+  }
 };
 
-const writeFile = (fileName, buffer) => {
-  fs.writeFile(fileName, buffer, (err) => {
-    if (err) console.log(err);
-    else console.log(`success to write file`);
-  });
+const syncIO = {
+  checkExist: (filePath, callback) => {
+    try {
+      fs.statSync(filePath);
+    } catch (err) {
+      console.log(`fs.stat err: ${err}`);
+      callback();
+    }
+  },
+
+  write: (filepathname, text) => {
+    try {
+      fs.writeFileSync(filepathname, text);
+    } catch (err) {
+      if (err) console.log(`fs.writeFile err: ${err}`);
+    }
+  },
+
+  readAsJSON: (filePath, callback) => {
+    try {
+      const buffer = fs.readFileSync(filePath, "utf8");
+      const json = JSON.parse(buffer);
+      if (json) callback(json);
+    } catch (err) {
+      console.log(`fs.readFile err: ${err}`);
+    }
+  },
+
+  createDir: (path) => {
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
+    }
+  },
 };
 
 const extension = (contentType) => {
@@ -64,48 +116,51 @@ const extension = (contentType) => {
   }
 };
 
-const promiseFileDownload = (res) => {
-  return new Promise((resolve, reject) => {
-    const options = { encoding: null };
+const promiseFileDownload = {
+  download: (res) => {
+    return new Promise((resolve, reject) => {
+      const options = { encoding: null };
 
-    if (!res.cover) {
-      reject({ error: `Cover is none; isbn: ${res.isbn}` });
-    }
-
-    request.get(res.cover, options, (error, response, body) => {
-      if (error) {
-        reject({ error: error });
-      } else {
-        const header = response.headers;
-        const data = {
-          isbn: res.isbn,
-          header: header,
-          body: body,
-        };
-
-        resolve(data);
+      if (!res.cover) {
+        reject({ error: `Cover is none; isbn: ${res.isbn}` });
       }
+
+      request.get(res.cover, options, (error, response, body) => {
+        if (error) {
+          reject({ error: error });
+        } else {
+          const header = response.headers;
+          const data = {
+            isbn: res.isbn,
+            header: header,
+            body: body,
+          };
+
+          resolve(data);
+        }
+      });
     });
+  },
+
+  success: (path) => (res) => {
+    const buffer = new Buffer.from(res.body);
+    const contentType = res.header["content-type"];
+
+    syncIO.createDir(path);
+    const fileName = path + res.isbn + extension(contentType);
+
+    writeFile(fileName, buffer);
+  },
+
+  fail: (res) => {
+    const errorMessage = res.error;
+    console.log(errorMessage);
+  },
+};
+
+const writeFile = (fileName, buffer) => {
+  fs.writeFile(fileName, buffer, (err) => {
+    if (err) console.log(err);
+    else console.log(`success to write file`);
   });
-};
-
-const promiseFileDownloadSuccess = (path) => (res) => {
-  const buffer = new Buffer.from(res.body);
-  const contentType = res.header["content-type"];
-
-  createDirectory(path);
-  const fileName = path + res.isbn + extension(contentType);
-
-  writeFile(fileName, buffer);
-};
-
-const promiseFileDownloadFail = (res) => {
-  const errorMessage = res.error;
-  console.log(errorMessage);
-};
-
-const createDirectory = (path) => {
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
-  }
 };
--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const Shinkan = require("./Shinkan");
 const openbd = new OpenBD();
 const shinkan = new Shinkan();
 
+const output = {
+  downloadImagePath: "public/downloadimage/",
+  generateJsonFile: "public/json/booklist.json",
+};
+
 (async () => {
-  openbd.imagePath = "public/downloadimage/";
+  openbd.imagePath = output.downloadImagePath;
 
   let isbns = shinkan.getShinkanData();
   isbns = isbns.slice(0, 4);
-  console.log(isbns)
+  console.log(isbns);
   const comicDatas = [];
 
   isbns.map(async (isbn) => {
     const comicData = await openbd.downloadCoverImage(isbn);
     comicDatas.push(comicData);
+
+    openbd.writeJSON(output.generateJsonFile, comicData);
   });
 
-  console.log(comicDatas)
+  console.log(comicDatas);
 })();
